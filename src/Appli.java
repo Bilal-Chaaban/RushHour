3ed0608bbe91c50793bec560a2ef20766f586146
Bilal-Chaaban <bilal.chaaban@edu.univ-fcomte.fr>
2016-11-25 10:26:29 +0100

    Affectation des images aux bouttons

--- src/Appli.java
+++ src/Appli.java
@@ -1,3 +1,4 @@
+
 public class Appli {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater( new Runnable() {
--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,17 +1,15 @@
-/**
- * Created by valenza-manjaro on 21/11/16.
- */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 public class Fenetre extends JFrame{
     protected JPanel panelBouton = new JPanel(new GridLayout(4, 1, 2, 10));
     protected JPanel panelPrincipale = new JPanel();
-    protected JButton bouton1 = new JButton("jouer");
-    protected JButton bouton2 = new JButton("option");
-    protected JButton bouton3 = new JButton("score");
-    protected JButton bouton4 = new JButton("aides");
+    protected JButton bouton1 = new JButton(new ImageIcon("image/Boutton_Jouer.png"));
+    protected JButton bouton2 = new JButton(new ImageIcon("image/Boutton_Option.png"));
+    protected JButton bouton3 = new JButton(new ImageIcon("image/Boutton_Score.png"));
+    protected JButton bouton4 = new JButton(new ImageIcon("image/Boutton_Aide.png"));
+    protected JLabel t = new JLabel("RUSH HOUR");
     public Fenetre(){
         setSize(400,590);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,11 +19,17 @@ public class Fenetre extends JFrame{
         creerMenu();
     }
     public void creerMenu(){
+        bouton1.setPreferredSize(new Dimension(360,100));
+        bouton2.setPreferredSize(new Dimension(360,100));
+        bouton3.setPreferredSize(new Dimension(360,100));
+        bouton4.setPreferredSize(new Dimension(360,100));
         panelBouton.add(bouton1);
         panelBouton.add(bouton2);
         panelBouton.add(bouton3);
         panelBouton.add(bouton4);
+        panelPrincipale.add(t);
         panelPrincipale.add(panelBouton);
+
         setContentPane(panelPrincipale);
     }
     public void setButtonControler(ActionListener listener) {
