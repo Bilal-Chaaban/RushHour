6119dcbe4625ffbdb77661ab063d9bf5f77043e4
LouisBannwarth <lb68580@gmail.com>
2016-12-02 14:06:31 +0100

    model getPos

--- src/Model.java
+++ src/Model.java
@@ -4,7 +4,14 @@
 public class Model {
 
     private Vehicule[][] vehicule =new Vehicule[6][6];
-
+    public Model(){}
+    public int[] getPosition(Vehicule v){
+        for (int i = 5; i >=0 ; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (v.equals(vehicule[i][j]));
+            }
+        }
+    }
     public void setVehicule(Vehicule[][] vehicule) {
         this.vehicule=vehicule;
     }
