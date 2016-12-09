ce1d7ce4c122250920c503eb75613adc37f4c428
LouisBannwarth <lb68580@gmail.com>
2016-12-09 10:42:46 +0100

    test vehicule

--- src/Model.java
+++ src/Model.java
@@ -1,45 +1,47 @@
+import java.io.*;
+
 /**
  * Created by lb685 on 02/12/2016.
  */
 public class Model {
 
-    private Vehicule[][] vehicule =new Vehicule[6][6];
+    private Vehicule[][] vehicule = new Vehicule[6][6];
 
-    public static final int HAUT=7;
-    public static final int BAS=8;
-    public static final int GAUCHE=9;
-    public static final int DROITE=10;
+    public static final int HAUT = 7;
+    public static final int BAS = 8;
+    public static final int GAUCHE = 9;
+    public static final int DROITE = 10;
 
-    public Model(int niveau){
+    public Model(int niveau) {
         init(niveau);
     }
 
-    public boolean isNull(int i,int j){
-        return vehicule[i][j]==null;
+    public boolean isNull(int i, int j) {
+        return vehicule[i][j] == null;
     }
 
-    public void deplacer (Vehicule v,int direction){
+    public void deplacer(Vehicule v, int direction) {
         int[] position = getPosition(v);
-        if ( (direction == 9 && position[1] != 0) ){
-            if(isNull(position[0], position[1]-1)) {
+        if ((direction == 9 && position[1] != 0)) {
+            if (isNull(position[0], position[1] - 1)) {
                 vehicule[position[0]][position[1] - 1] = v;
                 vehicule[position[0]][position[1] + v.taille - 1] = null;
             }
         }
-        if ( (direction == 10 && position[1] < (5 - v.taille)) ){
-            if(isNull(position[0], position[1] + v.taille - 1)) {
+        if ((direction == 10 && position[1] < (5 - v.taille))) {
+            if (isNull(position[0], position[1] + v.taille - 1)) {
                 vehicule[position[0]][position[1] + v.taille - 1] = v;
                 vehicule[position[0]][position[1]] = null;
             }
         }
-        if ( (direction == 7 && position[0] != 0) ){
-            if(isNull(position[0] - v.taille+1, position[1])) {
+        if ((direction == 7 && position[0] != 0)) {
+            if (isNull(position[0] - v.taille + 1, position[1])) {
                 vehicule[position[0] - v.taille + 1][position[1]] = v;
                 vehicule[position[0]][position[1]] = null;
             }
         }
-        if ( (direction == 8 && position[0] < (5 - v.taille-1)) ){
-            if(isNull(position[0] + 1, position[1])) {
+        if ((direction == 8 && position[0] < (5 - v.taille - 1))) {
+            if (isNull(position[0] + 1, position[1])) {
                 vehicule[position[0] + 1][position[1]] = v;
                 vehicule[position[0] - v.taille - 1][position[1]] = null;
             }
@@ -47,13 +49,13 @@ public class Model {
     }
 
 
-    public int[] getPosition(Vehicule v){
-        int[] tab=new int[2];
-        for (int i = 5; i >=0 ; i--) {
+    public int[] getPosition(Vehicule v) {
+        int[] tab = new int[2];
+        for (int i = 5; i >= 0; i--) {
             for (int j = 0; j < 6; j++) {
-                if (v.equals(vehicule[i][j])){
-                    tab[0]=i;
-                    tab[1]=j;
+                if (v.equals(vehicule[i][j])) {
+                    tab[0] = i;
+                    tab[1] = j;
                 }
             }
         }
@@ -61,7 +63,7 @@ public class Model {
     }
 
     public void setVehicule(Vehicule[][] vehicule) {
-        this.vehicule=vehicule;
+        this.vehicule = vehicule;
     }
 
     public Vehicule[][] getVehicule() {
@@ -72,23 +74,37 @@ public class Model {
         //lecture dans un fichier la position des vehicule en fct du niv passer en param
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                vehicule[i][j]=null;
+                vehicule[i][j] = null;
             }
         }
     }
 
-    public String toString(){
-        String ret="";
+    public String toString() {
+        String ret = "";
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 if (vehicule[i][j] == null) {
-                    ret+="| ";
-                }else {
-                    ret+="|"+vehicule[i][j].getCouleur();
+                    ret += "| ";
+                } else {
+                    ret += "|" + vehicule[i][j].getCouleur();
                 }
             }
-            ret+="|\n";
+            ret += "|\n";
         }
         return ret;
     }
+
+    public void saveBin(String fileName) throws IOException {
+        ObjectOutputStream oos = null;
+        FileOutputStream file = new FileOutputStream(fileName);
+        oos = new ObjectOutputStream(file);
+        oos.writeObject(this.vehicule);
+    }
+    public void loadBin(String fileName) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = null;
+        FileInputStream file = new FileInputStream(fileName);
+        ois = new ObjectInputStream(file);
+        if (ois!=null) vehicule= (Vehicule[][]) ois.readObject();
+    }
+
 }
