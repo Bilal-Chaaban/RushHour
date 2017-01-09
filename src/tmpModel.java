231a9df9ebe25134d551048e0b44e618b565e035
PierrV <pierre.v691@gmail.com>
2017-01-09 19:11:24 +0100

    Changement niveau 2 + voiture rouge dans le niveau 1

--- src/tmpModel.java
+++ src/tmpModel.java
@@ -7,11 +7,17 @@ public class tmpModel {
 
 
     public static void main(String[] args) {
-        Model model = new Model(1);
+        int x = 2;
+        Model model = new Model(x);
         Voiture v1 = new Voiture(Vehicule.BLEU);
-        Voiture v2 = new Voiture(Vehicule.ORANGE);
+        Voiture v2 = new Voiture(Vehicule.ROUGE);
         Voiture v3 = new Voiture(Vehicule.BRUN);
         Voiture v4 = new Voiture(Vehicule.JAUNE);
+        Voiture v5 = new Voiture(Vehicule.VERT);
+        Voiture v6 = new Voiture(Vehicule.VIOLET);
+        Voiture v7 = new Voiture(Vehicule.ORANGE);
+
+
         Camion  c1 = new Camion(Vehicule.VIOLET);
         Camion  c2 = new Camion(Vehicule.BRUN);
         Camion  c3 = new Camion(Vehicule.ROUGE);
@@ -22,7 +28,27 @@ public class tmpModel {
                 tab[i][j]=null;
             }
         }
-        tab[0][0] = v1;
+        tab[0][1] = v1;
+        tab[1][1] = v1;
+
+        tab[2][1] = v2;
+        tab[2][2] = v2;
+
+        tab[0][2] = v3;
+        tab[0][3] = v3;
+
+        tab[1][3] = v4;
+        tab[2][3] = v4;
+
+        tab[3][2] = v5;
+        tab[3][3] = v5;
+
+        tab[0][4] = v6;
+        tab[1][4] = v6;
+
+        tab[1][5] = v7;
+        tab[2][5] = v7;
+      /*  tab[0][0] = v1;
         tab[0][1] = v1;
 
         tab[2][1] = v2;
@@ -48,17 +74,17 @@ public class tmpModel {
 
         tab[5][2] = c4;
         tab[5][3] = c4;
-        tab[5][4] = c4;
+        tab[5][4] = c4;*/
         model.setVehicule(tab);
         System.out.println(model);
         try {
-            model.saveBin("niveau1");
+            model.saveBin("niveau"+x);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Model m2 = new Model(2);
+        Model m2 = new Model(x);
         try {
-            m2.loadBin("niveau1");
+            m2.loadBin("niveau"+x);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
@@ -73,7 +99,7 @@ public class tmpModel {
                 Controller controller = new Controller(fenetre);
                 fenetre.setModel(m2);
                 //controller.setModel(m2);
-                fenetre.niveau(1);
+                fenetre.niveau(x);
                 fenetre.setButtonControler(controller);
                 fenetre.menuReset();
 
